3aea570b954a32a58f0dbc5b297ce2f88d88340d
khanhmino <70557489+khanhmino@users.noreply.github.com>
2020-09-20 11:18:34 -0500

    menu UIs

---
+++ UT Game Jam 2020/Assets/Scripts/UI Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : Menu
+{
+    public GameObject pauseMenu;
+
+    public static bool isPaused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+}

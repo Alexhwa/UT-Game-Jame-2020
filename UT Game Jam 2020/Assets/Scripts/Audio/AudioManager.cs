455dc036189795e606b506fcdff6df8abc6c3903
khanhmino <70557489+khanhmino@users.noreply.github.com>
2020-09-19 23:24:38 -0500

    sounds, cutscene, menus

---
+++ UT Game Jam 2020/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource bgmAudio;
+    public AudioSource sfxAudio;
+    public AudioSource ambienceAudio;
+
+
+    public void PlayOneShot(AudioClip clip) {
+        sfxAudio.PlayOneShot(clip);
+    }
+
+    public void PlayLoop(AudioSource audioSource, AudioClip clip, float fadeTime) {
+        audioSource.DOKill();
+
+        audioSource.clip = clip;
+        audioSource.volume = 0;
+
+        audioSource.Play();
+        audioSource.DOFade(1, fadeTime);
+    }
+
+    public void StopLoop(AudioSource audioSource, float fadeTime) {
+        audioSource.DOKill();
+        audioSource.DOFade(0, fadeTime);
+    }
+}
